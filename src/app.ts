b0822e9793a47adb03f77156d34f9d1ce3aedc50
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-04-23 18:39:50 +0300

    Add enviorment variable

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./app";
 
-const PORT = 3000;
+const { PORT } = process.env;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
