d6a16b2fd01d6cdf76471559a2a8defbfd8e50e7
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-07-15 11:50:28 +0300

    Set logging to false

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,4 +4,4 @@ const { PG_DATABASE, PG_USERNAME, PG_PASSWORD, PG_HOST, PG_PORT } = process.env;
 
 const URI = `postgres://${PG_USERNAME}:${PG_PASSWORD}@${PG_HOST}:${PG_PORT}/${PG_DATABASE}`;
 
-export const db = new Sequelize(URI);
+export const db = new Sequelize(URI, { logging: false });
