63305df0f42a6b7a8f4b2a690c213e2feee36be2
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-07-15 12:55:45 +0300

    Create post route

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -1,11 +1,20 @@
 import { Response, Request } from "express";
-import logger from "src/config/logger";
+import logger from "../config/logger";
 import { Post } from "../models/Post";
 
-export const createPost = async (req: Request): Promise<void> => {
+export const createPost = async (req: Request, res: Response): Promise<void> => {
   try {
-    const newPost = await Post.create(req.body);
+    interface IPost {
+      message: string;
+    }
+
+    const post: IPost = {
+      message: req.body.message
+    };
+
+    const newPost = await Post.create(post);
     logger.debug(newPost);
+    res.json(newPost);
   } catch (err) {
     logger.error(err);
   }
