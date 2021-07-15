cc364478ec9a5ad44ecdd5180a10c37a681ea219
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-07-15 13:32:47 +0300

    Setup swagger

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import swaggerUI from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
+
 import logger from "./config/logger";
 import { db } from "./config/database";
 
@@ -21,6 +24,36 @@ const app = express();
 // Configure express
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Configure swagger
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "TS Node Starter API",
+      version: "1.0.0",
+      description: "TypeScript API boilerplate",
+      contact: {
+        name: "Maximilian Hagelstam",
+        email: "maximilian.hagelstam@gmail.com"
+      },
+      license: {
+        name: "MIT",
+        url: "https://opensource.org/licenses/MIT"
+      }
+    },
+    servers: [
+      {
+        url: "http://localhost:8080",
+        description: "Local server"
+      }
+    ]
+  },
+  apis: ["./src/api/v0/routes/*.js"]
+};
+
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 // Routes
 app.use("/post", postRouter);
