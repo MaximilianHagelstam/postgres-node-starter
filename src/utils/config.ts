6306c8f48305152967e3ff5e1afb311a561e72f1
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-11-01 18:18:09 +0200

    Add dev and prod db connection to config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,7 +3,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
+const DB_HOST = process.env.NODE_ENV === "production" ? process.env.DB_HOST_PROD : process.env.DB_HOST_DEV;
+const DB_PORT =
+    process.env.NODE_ENV === "production"
+        ? parseInt(<string>process.env.DB_PORT_PROD)
+        : parseInt(<string>process.env.DB_PORT_DEV);
 const DB_NAME = process.env.NODE_ENV === "production" ? process.env.DB_NAME_PROD : process.env.DB_NAME_DEV;
-const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_PASSWORD = process.env.NODE_ENV === "production" ? process.env.DB_PASSWORD_PROD : process.env.DB_PASSWORD_DEV;
+const DB_USERNAME = process.env.NODE_ENV === "production" ? process.env.DB_USERNAME_PROD : process.env.DB_USERNAME_DEV;
 
-export { PORT, DB_NAME, DB_PASSWORD };
+export { PORT, DB_HOST, DB_PORT, DB_NAME, DB_PASSWORD, DB_USERNAME };
--- src/utils/connectDb.ts
+++ src/utils/connectDb.ts
@@ -1,13 +1,15 @@
 import { createConnection } from "typeorm";
 import logger from "./logger";
-import { DB_NAME, DB_PASSWORD } from "./config";
+import { DB_HOST, DB_PORT, DB_NAME, DB_PASSWORD, DB_USERNAME } from "./config";
 
 const connectDb = async () => {
     try {
         await createConnection({
             type: "postgres",
+            host: DB_HOST,
+            port: DB_PORT,
             database: DB_NAME,
-            username: "postgres",
+            username: DB_USERNAME,
             password: DB_PASSWORD,
             logging: false,
             synchronize: true
