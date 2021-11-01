c84accaaf91fe8d6896846867b0fbb162e0656b2
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-11-01 14:42:30 +0200

    Add DB_URI to config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,5 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
+const DB_URI = process.env.NODE_ENV === "production" ? process.env.DB_URI_PROD : process.env.DB_URI_DEV;
 
-export { PORT };
+export { PORT, DB_URI };
